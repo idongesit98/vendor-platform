41f1ff38e3016ecaa137e635d11702be17adf76d
Idongesit Samuel Robson <robsonidongesitsamuel@gmail.com>
2026-03-01 22:56:15 +0100

    feat: Added Menu Entities (#2)

--- menuitems-service/src/modules/entities/menu-item.entity.ts
+++ menuitems-service/src/modules/entities/menu-item.entity.ts
@@ -3,33 +3,48 @@ import {
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
-  OneToMany,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
-import { UserOrder } from './user-order.entity';
+import { MenuCategory } from './menu-categories.entity';
 
 @Entity('menu_items')
 export class MenuItem {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   name: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   description: string;
 
   @Column('decimal', { precision: 10, scale: 2 })
   price: number;
 
   @Column()
-  category: string;
+  vendorId: string;
 
-  @Column({ default: true })
+  @ManyToOne(() => MenuCategory, (category) => category.menuItems)
+  @JoinColumn({ name: 'categoryId' })
+  category: MenuCategory;
+
+  @Column()
+  categoryId: string;
+
+  @Column({ default: false })
   isAvailable: boolean;
+
+  @Column()
+  imageUrl: string;
+
+  @Column({ default: 0 })
+  prepTime: number;
 
   @CreateDateColumn()
   createdAt: Date;
 
-  @OneToMany(() => UserOrder, (order) => order.menuItem)
-  orders: UserOrder[];
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
