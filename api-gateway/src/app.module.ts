a0b3fcd783eeee816520f26150ee45c78d64f67a
idongesit <robsonidongesitsamuel@gmail.com>
2026-03-02 09:25:59 +0100

    chore: removed unnecessary files

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -25,13 +25,11 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
     UserModule,
     MenuModule,
   ],
-  controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
     },
-    AppService,
   ],
 })
 export class AppModule implements NestModule {
